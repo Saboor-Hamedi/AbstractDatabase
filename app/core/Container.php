d5361b579e51f9c71824113c1adb767a515ecf20
Saboor-Hamedi <janhamedi49@gmail.com>
2024-07-02 09:56:07 +0700

    Validating Parameters
    still on the Container, securing the constructor, and container

--- app/core/Container.php
+++ app/core/Container.php
@@ -6,6 +6,7 @@ namespace AbstractDatabase\core;
 
 use AbstractDatabase\core\Exception\ContainerException;
 use ReflectionClass; // this is a default class of php
+use ReflectionNamedType;
 
 class Container
 {
@@ -16,17 +17,30 @@ class Container
     }
     public function resolve(string $className)
     {
+        // validation construction
         $reflectionClass = new ReflectionClass($className);
         if (!$reflectionClass->isInstantiable()) {
-            throw new ContainerException('Class' . $className . 'is not instantiable');
+            throw new ContainerException("Class {$className} is not instantiable");
         }
         $constructor = $reflectionClass->getConstructor();
         if (!$constructor) {
             return new $className;
         }
+
         $params = $constructor->getParameters();
         if (count($params) === 0) {
             return new $className;
+        }
+        $dependencies = [];
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+        }
+        if (!$type) {
+            throw new ContainerException("Failed to resolve class {$className} because param {$param} is missing a type hint.");
+        }
+        if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+            throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
         }
         dd($params);
     }
