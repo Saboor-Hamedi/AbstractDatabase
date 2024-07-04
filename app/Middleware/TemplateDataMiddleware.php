ddcdcd4eb9ae834ce142382ebf460f0237dc75ee
Saboor-Hamedi <janhamedi49@gmail.com>
2024-07-04 09:22:04 +0700

    loop
    inject the container into middlelware

--- app/Middleware/TemplateDataMiddleware.php
+++ app/Middleware/TemplateDataMiddleware.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace AbstractDatabase\Middleware;
 
 use AbstractDatabase\Contracts\MiddlewareInterface;
-
+use AbstractDatabase\core\TemplateEngine;
 
 class TemplateDataMiddleware implements MiddlewareInterface
 {
+    /**
+     * The goal is to acces $title within middleware
+     * support dependency injection in middleware
+     */
+    public function __construct(protected TemplateEngine $view)
+    {
+    }
     public function process(callable $nex)
     {
         echo 'I am TEMPATEDATA';
