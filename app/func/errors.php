5eaae17b056ef9b259ddb1b286108df5847cf2f7
Saboor-Hamedi <janhamedi49@gmail.com>
2024-07-05 11:25:43 +0700

    "Added EmailRule to ValidateService and updated validation rules for email field in ValidateRegister method."

--- app/func/errors.php
+++ app/func/errors.php
@@ -6,10 +6,10 @@ function error($errors, $field)
     if (isset($errors[$field])) {
         if (is_array($errors[$field])) {
             foreach ($errors[$field] as $error) {
-                echo '<small class="error">' . htmlspecialchars($error) . '</small><br>';
+                echo '<small class="error" style="color: red; font-size:10px; margin:0;padding:0px;">' . htmlspecialchars($error) . '</small><br>';
             }
         } else {
-            echo '<small class="error">' . htmlspecialchars($errors[$field]) . '</small>';
+            echo '<small class="error" style="color: red;font-size:10px; margin:0;padding:0px">' . htmlspecialchars($errors[$field]) . '</small>';
         }
     }
 }
